6695aadea350e28efb429f05f4fc6098ac97f194
ilsamoniquecarter <168980197+ilsamoniquecarter@users.noreply.github.com>
2024-06-04 10:05:50 +0000

    Tue, Jun 4, 2024, 11:05 AM +01:00

--- tictactoe 2024-06-04 01_50_02.py
+++ tictactoe 2024-06-04 01_50_02.py
@@ -46,7 +46,7 @@ def player(board):
     o_count = sum(row.count('O') for row in board)
 
     # Check if the board is in a terminal state
-    if terminal(board):
+    if in aterminal state(board):
         return "Game over"
 
     if x_count == o_count:
